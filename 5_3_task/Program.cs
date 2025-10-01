f51bcdff0b1cd79010c1d9a5edca5435c3b2bb37
Your Name <you@example.com>
2025-10-01 22:57:21 +0400

    5 task complete

---
+++ 5_3_task/Program.cs
@@ -0,0 +1,28 @@
+﻿//для перевода числа из двоичной системы счисления в десятичную
+using System;
+using System.ComponentModel;
+class Converter
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число в двоичной СС: ");
+        long num = long.Parse(Console.ReadLine());
+
+        Console.WriteLine(binary_converter(num));
+        Console.WriteLine(binary_converter_old(num));
+    }
+    static long binary_converter(long num)
+    {
+        if (num == 1) return 1L;
+        if (num == 0) return 0L;
+        return (binary_converter(num / 10) << 1) + binary_converter(num % 10);
+    }
+    static long binary_converter_old(long bin)
+    {
+        if (bin == 1) return 1;
+        if (bin == 0) return 0;
+        int digit_position = (int)Math.Log10(bin);
+        int power = (int)Math.Pow(10, digit_position);
+        return binary_converter_old(bin / power) * ((int)Math.Pow(2, digit_position)) + binary_converter_old(bin % power);
+    }
+}
---
+++ 5_4_task/Program.cs
@@ -0,0 +1,37 @@
+﻿class Stars
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите n: ");
+        //int n = int.Parse(Console.ReadLine());
+        int n = 12;
+        func(n, 0);
+    }
+    static void str(int n, char s)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(s);
+        }
+    }
+    static void func(int n, int i)
+    {
+        if (n == 0)
+        {
+            str(i * 2 + 2, '*');
+            Console.WriteLine();
+        }
+        if (n > 0)
+        {
+            str(i + 1, '*');
+            str(n, ' ');
+            str(i + 1, '*');
+            Console.WriteLine();
+            func(n - 2, i + 1);
+            str(i + 1, '*');
+            str(n, ' ');
+            str(i + 1, '*');
+            Console.WriteLine();
+        }
+    }
+}
